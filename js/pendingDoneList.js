db0f3f8e3defc02df7b7972ac210ff3043f40c2c
Eric Lin <eric.lin@ibm.com>
2022-01-10 17:53:29 +0800

    third commit

--- js/pendingDoneList.js
+++ js/pendingDoneList.js
@@ -28,8 +28,175 @@
                 });
             }, 2000);
         }
+        $(document).ready(createLineChart);
+      $(document).bind("kendo:skinChange", createLineChart);
+      $(document).ready(createBarChart);
+    $(document).bind("kendo:skinChange", createBarChart);
     });
+    function createLineChart() {
+              var month = moment().format("MMM");
+              var date = moment().format("DD");
+              var currentCapacityList = [];
+              var maxCapacityList = [];
+              var Linechartoptions={
+                transitions: false,
+                /*title: {
+                    text: "Current Capacity %"
+                },*/
+                legend: {
+                  position: "bottom"
+                },
+                chartArea: {
+                  background: ""
+                },
+                seriesDefaults: {
+                  type: "line",
+                  style: "smooth",
+                  markers: {
+                    visible: false,
+                    size: 3
+                  },
 
+                },
+                series: [{
+                  name: "Max Capacity",
+                  data: maxCapacityList,
+                  color: "red",
+                }, {
+                  name: "Current Capacity",
+                  data: currentCapacityList,
+                  color: "blue",
+                }],
+                valueAxis: {
+
+                  labels: {
+                    format: "{0}人"
+                  },
+                  line: {
+                    visible: false
+                  },
+                  axisCrossingValue: -10,
+                  title: {
+                    text: "容量(人)"
+                  },
+
+                },
+                categoryAxis: {
+                  title: {
+                    text: "時間"
+                  }
+                },
+                tooltip: {
+                  visible: true,
+                  format: "{0}%",
+                  template: "#= value #"
+                }
+              };
+              $("#capacityChart").kendoChart(Linechartoptions);
+
+              // 固定每0.5秒拿一次資料
+              var keepGoing = true;
+              var intervalId = window.setInterval(function () {
+                $.ajax({
+                  url: "http://localhost:8080/bank/loan/capacity",
+                  method: "GET",
+                  crossDomain: true,
+                  dataType: "json",
+                }).done(function (req) {
+                  console.log(req);
+                  if (keepGoing) {
+                    currentCapacityList.push(req.current * 1000);
+                    maxCapacityList.push(req.max * 1000);
+                    $("#capacityChart").data("kendoChart").refresh();
+                  }
+                  if (req.current == 0) {
+                    keepGoing = false;
+                  } else {
+                    keepGoing = true;
+                  }
+                });
+              }, 1000);
+          }
+          function createBarChart() {
+                    var month = moment().format("MMM");
+                    var date = moment().format("DD");
+                    var currentCapacityList = [];
+                    var maxCapacityList = [];
+                    var Linechartoptions={
+                      transitions: false,
+                      /*title: {
+                          text: "Current Capacity %"
+                      },*/
+                      legend: {
+                        position: "bottom"
+                      },
+                      chartArea: {
+                        background: ""
+                      },
+                      seriesDefaults: {
+                        type: "column",
+                        style: "smooth",
+                        markers: {
+                          visible: false,
+                          size: 3
+                        },
+
+                      },
+                      series: [ {
+                        name: "Current Capacity",
+                        data: currentCapacityList,
+                        color: "blue",
+                      }],
+                      valueAxis: {
+
+                        labels: {
+                          format: "{0}人"
+                        },
+                        line: {
+                          visible: false
+                        },
+                        axisCrossingValue: -10,
+                        title: {
+                          text: "容量(人)"
+                        },
+
+                      },
+                      categoryAxis: {
+                        title: {
+                          text: "時間"
+                        }
+                      },
+                      tooltip: {
+                        visible: true,
+                        format: "{0}%",
+                        template: "#= value #"
+                      }
+                    };
+                    $("#BarChart").kendoChart(Linechartoptions);
+
+                    // 固定每0.5秒拿一次資料
+                    var keepGoing = true;
+                    var intervalId = window.setInterval(function () {
+                      $.ajax({
+                        url: "http://localhost:8080/bank/loan/capacity",
+                        method: "GET",
+                        crossDomain: true,
+                        dataType: "json",
+                      }).done(function (req) {
+                        console.log(req);
+                        if (keepGoing) {
+                          currentCapacityList.push(req.current * 1000);
+                          //maxCapacityList.push(req.max * 1000);
+                          $("#BarChart").data("kendoChart").refresh();
+                        }
+                        if (req.current == 0) {
+                          keepGoing = false;
+                        } else {
+                          keepGoing = true;
+                        }
+                      });
+                    }, 1000);
+                }
     function getCapacity() {
         return new Promise((resolve, reject) => {
             $.ajax({
@@ -88,7 +255,7 @@
                     </label>\
                     <div class=\"d-flex mt-2\">\
                         <div class=\"ps-4 text-small me-3\">" + time + "</div>\
-                        <div class=\"badge badge-opacity-warning me-3\">Done</div>\
+                        <div class=\"badge badge-opacity-success me-3\">Done</div>\
                     </div>\
                 </div>\
             </li>";
@@ -105,5 +272,3 @@
     }
 
 })(jQuery);
-
-
